26060795e4e3bd33eb6acf68c5675e01d4fd86e2
Torgon3D <torgon99@gmail.com>
2026-03-09 13:34:43 +0100

    Some more register work

--- RiscVInterpreter/InterpreterEngine/Interpreter.cs
+++ RiscVInterpreter/InterpreterEngine/Interpreter.cs
@@ -2,5 +2,13 @@
 
 class Interpreter
 {
-    
+    public enum EInstructionFormat
+    {
+        R,
+        I,
+        S,
+        B,
+        U,
+        J
+    }
 }
--- RiscVInterpreter/InterpreterEngine/Register.cs
+++ RiscVInterpreter/InterpreterEngine/Register.cs
@@ -1,5 +1,8 @@
 
 
+using System;
+using System.ComponentModel;
+
 public class Register
 {
     byte[] registerValue;
@@ -8,4 +11,64 @@ public class Register
     {
         registerValue = new byte[size];
     }
+    
+    public int GetAsInt32()
+    {
+        return BitConverter.ToInt32(registerValue);
+    }
+    
+    public void SetFromInt32(int value)
+    {
+        registerValue = BitConverter.GetBytes(value);
+    }
+    
+    public uint GetAsUInt32()
+    {
+        return BitConverter.ToUInt32(registerValue);
+    }
+    
+    public void SetFromUInt32(uint value)
+    {
+        registerValue = BitConverter.GetBytes(value);
+    }
+    
+    public short GetAsInt16()
+    {
+        return BitConverter.ToInt16(registerValue);
+    }
+    
+    public void SetFromInt16(short value)
+    {
+        registerValue = BitConverter.GetBytes(value);
+    }
+    
+    public ushort GetAsUInt16()
+    {
+        return BitConverter.ToUInt16(registerValue);
+    }
+    
+    /* public void SetFromUInt16(ushort value)
+    {
+        registerValue = BitConverter.GetBytes(value);
+    }
+    
+    public int GetAsInt8()
+    {
+        return BitConverter.To(registerValue);
+    }
+    
+    public void SetFromInt8(int value)
+    {
+        registerValue = BitConverter.GetBytes(value);
+    }
+    
+    public uint GetAsUInt8()
+    {
+        return BitConverter.ToUInt32(registerValue);
+    }
+    
+    public void SetFromUInt8(byte value)
+    {
+        registerValue[0] = value;
+    } */
 }
