8c2338cb8330814865dcc1bb682921bde0deb835
Torgon3D <torgon99@gmail.com>
2026-03-25 11:46:29 +0100

    New idea or how to add instructionsets

--- RiscVInterpreter/InterpreterEngine/Interpreter.cs
+++ RiscVInterpreter/InterpreterEngine/Interpreter.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+// Kansje endre til class som holder alle instruksjonformater. Kansje som partial
     public enum EInstructionFormat
     {
         R,
@@ -9,7 +10,19 @@ using System.Collections.Generic;
         S,
         B,
         U,
-        J
+        J,
+        fR,
+        fI,
+        fS,
+        fB,
+        fU,
+        fJ,
+        pR,
+        pI,
+        pS,
+        pB,
+        pU,
+        pJ,
     }
 public class Interpreter
 {
@@ -57,7 +70,7 @@ public class RiscVInstruction
     
     public void RunFunction(RiscVArguments args)
     {
-        if (!args.IsInstructionformatCorrect(_instructionFormat)) return; // TODO
+        if (!args.GetArgumentInfo(_instructionFormat)) return; // TODO
         
         _instructionFunction.Invoke(args);
     }
@@ -72,30 +85,28 @@ public class RiscVArguments
         
     }
     
-    public bool IsInstructionformatCorrect(EInstructionFormat format)
+    public byte GetArgumentInfo(EInstructionFormat format)
     {
-        switch (format)
-        {
-        case EInstructionFormat.R:
-            return rd != null && rs1 != null && rs2 != null;
-            
-        case EInstructionFormat.I:
-            return rd != null && rs1 != null && imm != null;
-            
-        case EInstructionFormat.S:
-            return rs1 != null && rs2 != null && imm != null;
-            
-        case EInstructionFormat.B:
-            return rs1 != null && rs2 != null && imm != null;
-            
-        case EInstructionFormat.U:
-            return rd != null && imm != null;
-            
-        case EInstructionFormat.J:
-            return rd != null && imm != null;
-            
-        default:
-            return false;
-        }
+        byte returnval = 0;
+        
+        returnval += (byte)((rd != null ? 1 : 0) << 0);
+        returnval += (byte)((rs1 != null ? 1 : 0) << 1);
+        returnval += (byte)((rs2 != null ? 1 : 0) << 2);
+        returnval += (byte)((imm != null ? 1 : 0) << 3);
+        
+        return returnval;
+    }
+}
+// Include all the base information and checker functions that are needed
+public abstract class InstructionsetBase
+{
+    public Dictionary<string, RiscVInstruction> Instructions = new();
+}
+// Do this for every implementation in their own file
+public partial class InstructionsetImplementations : InstructionsetBase
+{
+    InstructionsetImplementations()
+    {
+        Instructions.
     }
 }
